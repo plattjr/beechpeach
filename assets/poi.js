2c39d06426addae05f88d8372bbe37ca3724e170
plattjr <70535541+plattjr@users.noreply.github.com>
2022-10-04 15:32:42 -0400

    Add files via upload

---
+++ assets/poi.js
@@ -0,0 +1,107 @@
+var poi = {
+  "type": "FeatureCollection",
+  "features": [
+    {
+      "type": "Feature",
+      "properties": {
+        "marker-color": "#44B2E5",
+        "marker-size": "medium",
+        "marker-symbol": 1,
+        name: "Laurel Creek Falls",
+        text: "Take a dip in Laurel Creek Falls (aka Trashcan Falls) to cool off!",
+        img: "./images/laurel.jpg",},
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -81.8527901172638,
+          36.25235452525927
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "marker-color": "#44B2E5",
+        "marker-size": "medium",
+        "marker-symbol": "",
+        name: "Bombing Gravel",
+        text: "5 mile gravel descent of Andy Hicks Road and onto Perry Presnell Road.",
+        img: "./images/gravel.jpg",},
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -81.86724185943604,
+          36.23053125768246
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "marker-color": "#44B2E5",
+        "marker-size": "medium",
+        "marker-symbol": "",
+        name: "Fred's General Mercantile",
+        text: "Gassin' up.  Grab a mid ride snack or meal from the Deli.",
+        img: "./images/gas.jpg",},
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -81.87354505062103,
+          36.187702055678926
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "marker-color": "#44B2E5",
+        "marker-size": "medium",
+        "marker-symbol": "",
+        name: "View of Grandfather",
+        text: "The descent off of Beech follows the rough and at times technical Elderberry Ridge Rd. Then opens up to the best view on the ride.",
+        img: "./images/beech.jpg",},
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -81.84962511062622,
+          36.187004994643594
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "marker-color": "#44B2E5",
+        "marker-size": "medium",
+        "marker-symbol": "",
+        name: "Ward's Mill Dam",
+        text: "Former site of the Ward's Mill Dam. Want to see what it looks like removed? Then hop on this ride and find out!",
+        img: "./images/dam.jpg",},
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -81.83040976524353,
+          36.24133116192736
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "marker-color": "#44B2E5",
+        "marker-size": "medium",
+        "marker-symbol": "",
+        name: "Cloud Inversion",
+        text: "Early morning starts mean you can often catch temperature inversion in the below Watauga River Valley.",
+        img: "./images/andy.jpg",},
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -81.86415195465088,
+          36.22056079122332
+        ]
+      }
+    }
+  ]
+}
